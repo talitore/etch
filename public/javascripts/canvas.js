9ece61f105e0c597965a1a19fb2b1cc7f4510e68
Taylor E Kems <taylor.kems@generalassemb.ly>
2015-02-11 01:01:03 -0600

    Full nodejs app now, with connect/disconnect events

--- public/javascripts/canvas.js
+++ public/javascripts/canvas.js
@@ -1,28 +1,45 @@
 // $ ->
 document.addEventListener("DOMContentLoaded", function(event) {
-  var canvas = document.getElementById('canvas');
+  canvas = document.getElementById('canvas');
   canvas.width=800;
   canvas.height=600;
 
-  var ctx = canvas.getContext('2d');
+  ctx = canvas.getContext('2d');
   ctx.fillStyle = 'rgb(200,200,200)';
-  ctx.fillRect(0,0,600,480);
+  ctx.fillRect(0,0,canvas.width,canvas.height);
   ctx.fillStyle = 'rgb(0,0,0)';
 
   canvas.addEventListener('mousedown', startDraw);
   canvas.addEventListener('mousemove', draw);
   canvas.addEventListener('mouseup', endDraw);
-
-  var drawing = false;
-  function startDraw(e) { drawing = true; }
-  function endDraw(e) { drawing = false; }
-
-  var coords = [];
-  function draw(e) {
-    if(!drawing) return;
-
-    var x = e.x-e.target.offsetLeft;
-    var y = e.y-e.target.offsetTop;
-    // ctx.fillRect(x,y,4,4);
-  }
 });
+
+var canvas;
+var ctx;
+var drawing = false;
+var coords = [];
+
+function startDraw(e) { drawing = true; }
+function endDraw(e) {
+  drawing = false;
+  coords = [];
+}
+var draw = function (e) {
+  if(!drawing) return;
+
+  var x = e.offsetX
+  var y = e.offsetY
+
+  coords.push({x: x, y: y});
+  socket.emit('originInfo', coords);
+  drawOnCanvas(coords);
+}
+function drawOnCanvas(coords) {
+  ctx.beginPath();
+  ctx.moveTo(coords[0].x, coords[0].y);
+
+  for(var i = 1; i < coords.length; i++) {
+    ctx.lineTo(coords[i].x, coords[i].y);
+  }
+  ctx.stroke();
+}
--- public/javascripts/socket.js
+++ public/javascripts/socket.js
@@ -1,1 +1,16 @@
-var socket = io();
+// $ ->
+document.addEventListener("DOMContentLoaded", function(event) {
+  socket = io();
+
+  socket.on('drawingFromOthers', function(params) {
+     drawOnCanvas(params);
+  });
+  socket.on('userConnect', function(params) {
+    console.log('ID: ' + params.id + ' connected.')
+  });
+  socket.on('userDisconnect', function(params) {
+    console.log('ID: ' + params.id + ' disconnected.')
+  });
+});
+
+var socket;
